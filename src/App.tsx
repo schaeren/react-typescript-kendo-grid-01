a3b068c43abf7fbba6bbef3a137e436ef79ef2f2
schaeren <peter.schaeren@swissteach.ch>
2019-05-18 19:47:25 +0200

    Initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Grid, GridColumn, GridSortChangeEvent} from '@progress/kendo-react-grid';
+//import '@progress/kendo-theme-default/dist/all.css';
+// used for sorting grid column 
+import { orderBy, SortDescriptor} from '@progress/kendo-data-query';
+//import {GridSortChangeEvent} from '@progress/'
+
+interface AppProps { }
+interface AppState {
+  products: {name: string; price: number;}[],
+  sort: SortDescriptor[]
+}
+
+class App extends Component<AppProps, AppState> {
+  private appName: string = 'My first app with React, TypeScript, KendoReact Grid';
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      products: [
+        { name: "Product One", price: 10 },
+        { name: "Product Two", price: 20 },
+        { name: "Product Three", price: 30 },
+        { name: "Product Four", price: 39.99 }
+      ],
+      sort: [
+        {field: 'price', dir: 'asc'} as SortDescriptor
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.appName}</h1>
+        <Grid data={orderBy(this.state.products, this.state.sort)} sortable sort={this.state.sort} onSortChange={(e)=>this.onSortChanged(e)}>
+          <GridColumn field="name" title="Product name"/>
+          <GridColumn field="price" title="Price"/>
+        </Grid>
+      </div>
+    );
+  }
+
+  public onSortChanged(event: GridSortChangeEvent): void {
+    for (let sortDescriptor of event.sort) {
+      console.log(`Sort ${sortDescriptor.field} ${sortDescriptor.dir}`);
+      this.setState({sort : event.sort});
+    }
+  }
 }
 
 export default App;
